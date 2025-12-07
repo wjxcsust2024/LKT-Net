b04fca2cca6996b2431ba19e72be634273fd74a7
wjxcsust2024 <158576104+wjxcsust2024@users.noreply.github.com>
2025-12-07 09:30:48 +0800

    Update block.py

--- block.py
+++ block.py
@@ -1,9 +1,42 @@
+import numpy
 from timm.models.layers import DropPath
-import numpy as np
 from ecb import SeqConv3x3
 from norms import *
-from deformabled import DeformableConv2d
-import torch.nn.functional as F
+from Separable_convolution import S_conv
+from torch.nn.init import trunc_normal_
+
+def attention_pool(tensor, pool, hw_shape, has_cls_embed=True, norm=None):
+    if pool is None:
+        return tensor, hw_shape
+    tensor_dim = tensor.ndim
+    if tensor_dim == 4:
+        pass
+    elif tensor_dim == 3:
+        tensor = tensor.unsqueeze(1)
+    else:
+        raise NotImplementedError(f"Unsupported input dimension {tensor.shape}")
+
+    if has_cls_embed:
+        cls_tok, tensor = tensor[:, :, :1, :], tensor[:, :, 1:, :]
+
+    B, N, L, C = tensor.shape
+    H, W = hw_shape
+    tensor = tensor.reshape(B * N, H, W, C).permute(0, 3, 1, 2).contiguous()
+
+    tensor = pool(tensor)
+
+    hw_shape = [tensor.shape[2], tensor.shape[3]]
+    L_pooled = tensor.shape[2] * tensor.shape[3]
+    tensor = tensor.reshape(B, N, C, L_pooled).transpose(2, 3)
+    if has_cls_embed:
+        tensor = torch.cat((cls_tok, tensor), dim=2)
+    if norm is not None:
+        tensor = norm(tensor)
+
+    if tensor_dim == 3:
+        tensor = tensor.squeeze(1)
+    return tensor, hw_shape
+
 
 class DWConv(nn.Module):
     def __init__(self, dim=768):
@@ -14,8 +47,22 @@ class DWConv(nn.Module):
         x = self.dwconv(x)
         return x
 
+
+class DepthWiseConv2d(nn.Module):
+    def __init__(self, dim_in, kernel_size=3, padding=1, stride=1, dilation=1):
+            super().__init__()
+
+            self.conv1 = nn.Conv2d(dim_in, dim_in, kernel_size=kernel_size, padding=padding,
+                                   stride=stride, dilation=dilation, groups=dim_in)
+            self.norm_layer = nn.GroupNorm(4, dim_in)
+            self.conv2 = nn.Conv2d(dim_in, dim_in, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv2(self.norm_layer(self.conv1(x)))
+
+
 class MultiScaleDWConv(nn.Module):
-    def __init__(self, dim, scale=( 3, 5, 7, 9)):
+    def __init__(self, dim, scale=(3, 5, 7, 9,11)):
         super().__init__()
         self.scale = scale
         self.channels = []
@@ -54,6 +101,7 @@ class IRB(nn.Module):
             nn.Conv2d(hidden_features, hidden_features, kernel_size=(1, 3), stride=(1, 1), padding=(0, (3 - 1) // 2),groups=hidden_features),
             nn.GELU(),
             nn.Conv2d(hidden_features, hidden_features, kernel_size=(3, 1), stride=(1, 1), padding=((3 - 1) // 2, 0),groups=hidden_features))
+
         #self.conv = DepthWiseConv2d(hidden_features)
         self.conv = nn.Sequential(
             nn.Conv2d(hidden_features, hidden_features, kernel_size=(1, 5), stride=(1, 1), padding=(0, (5 - 1) // 2),groups=hidden_features),
@@ -103,119 +151,385 @@ class ChannelAttention(nn.Module):
         out = self.sigmoid(y1+y2)*x
         return out
 
+
+def cal_rel_pos_spatial(
+    attn,
+    q,
+    has_cls_embed,
+    q_shape,
+    k_shape,
+    rel_pos_h,
+    rel_pos_w,
+):
+    """
+    Spatial Relative Positional Embeddings.
+    """
+    sp_idx = 1 if has_cls_embed else 0
+    q_h, q_w = q_shape
+    k_h, k_w = k_shape
+
+    # Scale up rel pos if shapes for q and k are different.
+    q_h_ratio = max(k_h / q_h, 1.0)
+    k_h_ratio = max(q_h / k_h, 1.0)
+    dist_h = (
+        torch.arange(q_h)[:, None] * q_h_ratio - torch.arange(k_h)[None, :] * k_h_ratio
+    )
+    dist_h += (k_h - 1) * k_h_ratio
+    q_w_ratio = max(k_w / q_w, 1.0)
+    k_w_ratio = max(q_w / k_w, 1.0)
+    dist_w = (
+        torch.arange(q_w)[:, None] * q_w_ratio - torch.arange(k_w)[None, :] * k_w_ratio
+    )
+    dist_w += (k_w - 1) * k_w_ratio
+
+    Rh = rel_pos_h[dist_h.long()]
+    Rw = rel_pos_w[dist_w.long()]
+
+    B, n_head, q_N, dim = q.shape
+
+    r_q = q[:, :, sp_idx:].reshape(B, n_head, q_h, q_w, dim)
+    rel_h = torch.einsum("byhwc,hkc->byhwk", r_q, Rh)
+    rel_w = torch.einsum("byhwc,wkc->byhwk", r_q, Rw)
+
+    attn[:, :, sp_idx:, sp_idx:] = (
+        attn[:, :, sp_idx:, sp_idx:].view(B, -1, q_h, q_w, k_h, k_w)
+        + rel_h[:, :, :, :, :, None]
+        + rel_w[:, :, :, :, None, :]
+    ).view(B, -1, q_h * q_w, k_h * k_w)
+
+    return attn
+
 class LKA(nn.Module):
     def __init__(self, dim):
         super().__init__()
-        self.conv = nn.Conv2d(dim, dim, 3, padding=1, groups=dim)
+        #self.conv = nn.Conv2d(dim, dim, 3, padding=1, groups=dim)
         self.conv0 = nn.Conv2d(dim, dim, 5, padding=5 // 2, groups=dim)
         self.conv_spatial = nn.Conv2d(dim, dim, 7, stride=1, padding=9, groups=dim, dilation=3)
         self.conv1 = nn.Conv2d(dim, dim, 1)
 
     def forward(self, x):
         u = x.clone()
-        attn = self.conv0(self.conv(x))
+        attn = self.conv0(x)
         attn = self.conv_spatial(attn)
         attn = self.conv1(attn)
-
         return u * attn
 
-class PoolingAttention(nn.Module):
-    def __init__(self, dim, num_heads=2, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.,
-                 pool_ratios=[1, 2, 3, 6]):
+class MultiScaleAttention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        dim_out,
+        input_size,
+        num_heads=8,
+        qkv_bias=False,
+        kernel_q=(1, 1),
+        kernel_kv=(1, 1),
+        stride_q=(1, 1),
+        stride_kv=(1, 1),
+        norm_layer=nn.LayerNorm,
+        has_cls_embed=True,
+        mode="conv",
+        pool_first=False,
+        rel_pos_spatial=False,
+        rel_pos_zero_init=False,
+        residual_pooling=True,
+    ):
         super().__init__()
-        assert dim % num_heads == 0, f"dim {dim} should be divided by num_heads {num_heads}."
+        self.pool_first = pool_first
 
-        self.dim = dim
         self.num_heads = num_heads
-        self.num_elements = np.array([t * t for t in pool_ratios]).sum()
-        head_dim = dim // num_heads
-        self.scale = qk_scale or head_dim ** -0.5
-
-        self.q = nn.Sequential(nn.Linear(dim, dim, bias=qkv_bias))
-        self.kv = nn.Sequential(nn.Linear(dim, dim * 2, bias=qkv_bias))
-        self.attn_drop = nn.Dropout(attn_drop)
-        self.proj = nn.Linear(dim, dim)
-        self.proj_drop = nn.Dropout(proj_drop)
-        self.ca = ChannelAttention(dim, 30)
+        self.dim_out = dim_out
+        head_dim = dim_out // num_heads
+        self.scale = head_dim**-0.5
+        self.has_cls_embed = has_cls_embed
+        padding_q = [int(q // 2) for q in kernel_q]
+        padding_kv = [int(kv // 2) for kv in kernel_kv]
         self.LKA = LKA(dim)
-        self.lepe = nn.Sequential(nn.Conv2d(dim, dim, kernel_size=(1, 3), stride=(1,1), padding=(0,1), groups=dim),
-                                                    nn.GELU(),
-                                                    nn.Conv2d(dim, dim, kernel_size=(3, 1), stride=(1,1), padding=(1,0), groups=dim),)
-        self.conv3 = DeformableConv2d(dim, dim, 3, padding=1)
-        self.bn = BatchChannelNorm(dim)
-        self.pool_ratios = pool_ratios
-        self.pools = nn.ModuleList()
-        self.norm = nn.LayerNorm(dim)
-        self.d_convs = nn.ModuleList([nn.Sequential(nn.Conv2d(dim, dim, kernel_size=(1, 7), stride=(1,1), padding=(0,9), groups=dim, dilation=3),
-                                                    nn.GELU(),
-                                                    nn.Conv2d(dim, dim, kernel_size=(7, 1), stride=(1,1), padding=(9,0), groups=dim, dilation=3),)
-                                                    for temp in pool_ratios])
+        self.Conv = nn.Conv2d(dim, dim, 3, padding=3 // 2, groups=dim)
+        if pool_first:
+            self.q = nn.Linear(dim, dim_out, bias=qkv_bias)
+            self.k = nn.Linear(dim, dim_out, bias=qkv_bias)
+            self.v = nn.Linear(dim, dim_out, bias=qkv_bias)
+        else:
+            self.qkv = nn.Linear(dim, dim_out * 2, bias=qkv_bias)
 
-    def forward(self, x, H, W):
+        self.proj = nn.Linear(dim_out, dim_out)
+
+        # Skip pooling with kernel and stride size of (1, 1, 1).
+        if numpy.prod(kernel_q) == 1 and numpy.prod(stride_q) == 1:
+            kernel_q = ()
+        if numpy.prod(kernel_kv) == 1 and numpy.prod(stride_kv) == 1:
+            kernel_kv = ()
+        self.mode = mode
+
+        if mode in ("avg", "max"):
+            pool_op = nn.MaxPool2d if mode == "max" else nn.AvgPool2d
+            self.pool_q = (
+                pool_op(kernel_q, stride_q, padding_q, ceil_mode=False)
+                if len(kernel_q) > 0
+                else None
+            )
+            self.pool_k = (
+                pool_op(kernel_kv, stride_kv, padding_kv, ceil_mode=False)
+                if len(kernel_kv) > 0
+                else None
+            )
+            self.pool_v = (
+                pool_op(kernel_kv, stride_kv, padding_kv, ceil_mode=False)
+                if len(kernel_kv) > 0
+                else None
+            )
+        elif mode == "conv" or mode == "conv_unshared":
+            if pool_first:
+                dim_conv = dim // num_heads if mode == "conv" else dim
+            else:
+                dim_conv = dim_out // num_heads if mode == "conv" else dim_out
+            self.pool_q = (
+                nn.Conv2d(
+                    dim_conv,
+                    dim_conv,
+                    kernel_q,
+                    stride=stride_q,
+                    padding=padding_q,
+                    groups=dim_conv,
+                    bias=False,
+                )
+                if len(kernel_q) > 0
+                else None
+            )
+            self.norm_q = norm_layer(dim_conv) if len(kernel_q) > 0 else None
+            self.pool_k = (
+                nn.Conv2d(
+                    dim_conv,
+                    dim_conv,
+                    kernel_kv,
+                    stride=stride_kv,
+                    padding=padding_kv,
+                    groups=dim_conv,
+                    bias=False,
+                )
+                if len(kernel_kv) > 0
+                else None
+            )
+            self.norm_k = norm_layer(dim_conv) if len(kernel_kv) > 0 else None
+            self.pool_v = (
+                nn.Conv2d(
+                    dim_conv,
+                    dim_conv,
+                    kernel_kv,
+                    stride=stride_kv,
+                    padding=padding_kv,
+                    groups=dim_conv,
+                    bias=False,
+                )
+                if len(kernel_kv) > 0
+                else None
+            )
+            self.norm_v = norm_layer(dim_conv) if len(kernel_kv) > 0 else None
+        else:
+            raise NotImplementedError(f"Unsupported model {mode}")
+
+        # relative pos embedding
+        self.rel_pos_spatial = rel_pos_spatial
+        if self.rel_pos_spatial:
+            assert input_size[0] == input_size[1]
+
+            size = input_size[0]
+            q_size = size // stride_q[1] if len(stride_q) > 0 else size
+            kv_size = size // stride_kv[1] if len(stride_kv) > 0 else size
+            rel_sp_dim = 2 * max(q_size, kv_size) - 1
+
+            self.rel_pos_h = nn.Parameter(torch.zeros(rel_sp_dim, head_dim))
+            self.rel_pos_w = nn.Parameter(torch.zeros(rel_sp_dim, head_dim))
+
+            if not rel_pos_zero_init:
+                trunc_normal_(self.rel_pos_h, std=0.02)
+                trunc_normal_(self.rel_pos_w, std=0.02)
+
+        self.residual_pooling = residual_pooling
+
+    def forward(self, x, hw_shape):
         B, N, C = x.shape
-        x_ = x.permute(0, 2, 1).reshape(B, C, H, W)
-        x_1 = self.LKA(x_).reshape(B, C, -1).permute(0, 2, 1)
-        # print(q_x.shape)
+        H = W = int(N ** 0.5)
+        x_0 = x.reshape(B, H, W, C).permute(0, 3, 1, 2)
+        x = self.Conv(x_0).permute(0, 2, 3, 1).reshape(B, N, C)
+        q = self.LKA(x_0).permute(0, 2, 3, 1).reshape(B, N, C)
+        if self.pool_first:
+            if self.mode == "conv_unshared":
+                fold_dim = 1
+            else:
+                fold_dim = self.num_heads
+            x_1 = x.reshape(B, N, fold_dim, -1).permute(0, 2, 1, 3)
+            k = v = x_1
+        else:
+            assert self.mode != "conv_unshared"
 
-        q = self.q(x_1).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
-        pools = []
-        x_2 = self.conv3(self.ca(x_))
-        for (pool_ratio, l) in zip(self.pool_ratios, self.d_convs):
-            pool = F.adaptive_avg_pool2d(x_2, (round(H / pool_ratio), round(W / pool_ratio)))
-            pool = pool + l(pool)
-            pools.append(pool.view(B, C, -1))
+            qkv = (self.qkv(x).reshape(B, N, 2, self.num_heads, -1).permute(2, 0, 3, 1, 4))
+            k, v = qkv[0], qkv[1]
 
-        pools = torch.cat(pools, dim=2)
-        pools = self.norm(pools.permute(0, 2, 1))
+        q = q.reshape(B, N, self.num_heads, -1).permute(0, 2, 1, 3)
+        q, q_shape = attention_pool(
+            q,
+            self.pool_q,
+            hw_shape,
+            has_cls_embed=self.has_cls_embed,
+            norm=getattr(self, "norm_q", None),
+        )
+        k, k_shape = attention_pool(
+            k,
+            self.pool_k,
+            hw_shape,
+            has_cls_embed=self.has_cls_embed,
+            norm=getattr(self, "norm_k", None),
+        )
+        v, v_shape = attention_pool(
+            v,
+            self.pool_v,
+            hw_shape,
+            has_cls_embed=self.has_cls_embed,
+            norm=getattr(self, "norm_v", None),
+        )
 
-        kv = self.kv(pools).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
-        k, v = kv[0], kv[1]
-        # print(k.shape)
-        q = torch.nn.functional.normalize(q, dim=-1)
-        k = torch.nn.functional.normalize(k, dim=-1)
+        if self.pool_first:
+            q_N = numpy.prod(q_shape) + 1 if self.has_cls_embed else numpy.prod(q_shape)
+            k_N = numpy.prod(k_shape) + 1 if self.has_cls_embed else numpy.prod(k_shape)
+            v_N = numpy.prod(v_shape) + 1 if self.has_cls_embed else numpy.prod(v_shape)
 
-        attn = (q @ k.transpose(-2, -1)) * self.scale
+            q = q.permute(0, 2, 1, 3).reshape(B, q_N, -1)
+            q = self.q(q).reshape(B, q_N, self.num_heads, -1).permute(0, 2, 1, 3)
+
+            v = v.permute(0, 2, 1, 3).reshape(B, v_N, -1)
+            v = self.v(v).reshape(B, v_N, self.num_heads, -1).permute(0, 2, 1, 3)
+
+            k = k.permute(0, 2, 1, 3).reshape(B, k_N, -1)
+            k = self.k(k).reshape(B, k_N, self.num_heads, -1).permute(0, 2, 1, 3)
+
+        N = q.shape[2]
+        attn = (q * self.scale) @ k.transpose(-2, -1)
+        if self.rel_pos_spatial:
+            attn = cal_rel_pos_spatial(
+                attn,
+                q,
+                self.has_cls_embed,
+                q_shape,
+                k_shape,
+                self.rel_pos_h,
+                self.rel_pos_w,
+            )
+
         attn = attn.softmax(dim=-1)
+        x = attn @ v
 
-        x = (attn @ v)
-        x = x.transpose(1, 2).contiguous().reshape(B, N, C)
-        q_2 = x_1.permute(0, 2, 1).reshape(B, C, H, W)
-        x = x + self.lepe(q_2).permute(0, 2, 3, 1).reshape(B, N, C)
+        if self.residual_pooling:
+            if self.has_cls_embed:
+                x[:, :, 1:, :] += q[:, :, 1:, :]
+            else:
+                x = x + q
+
+        x = x.transpose(1, 2).reshape(B, -1, self.dim_out)
+
         x = self.proj(x)
 
-        return x
+        return x, q_shape
 
 
 class Block(nn.Module):
 
-    def __init__(self, dim, num_heads=8, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
-                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, layerscale_value=1e-4, pool_ratios=[1, 2, 3, 6]):
+    def __init__(
+            self,
+            dim,
+            dim_out,
+            num_heads=1,
+            input_size=[56, 56],
+            mlp_ratio=4.0,
+            qkv_bias=False,
+            drop_path=0.0,
+            norm_layer=nn.LayerNorm,
+            kernel_q=(1, 1),
+            kernel_kv=(1, 1),
+            stride_q=(1, 1),
+            stride_kv=(1, 1),
+            mode="conv",
+            has_cls_embed=True,
+            pool_first=False,
+            rel_pos_spatial=False,
+            rel_pos_zero_init=False,
+            residual_pooling=True,
+            dim_mul_in_att=False,
+    ):
         super().__init__()
+        self.dim = dim
+        self.dim_out = dim_out
         self.norm1 = norm_layer(dim)
-        self.attn = PoolingAttention(
-            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
-            attn_drop=attn_drop, proj_drop=drop, pool_ratios=pool_ratios)
+        self.dim_mul_in_att = dim_mul_in_att
 
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-        #self.mlp = FastLeFF(dim)
-        self.norm2 = norm_layer(dim)
-        self.mlp = IRB(in_features=dim, hidden_features=int(dim * mlp_ratio), act_layer=nn.Hardswish, drop=drop,
-                       ksize=3)
-        self.gamma = nn.Parameter(layerscale_value * torch.ones((dim)), requires_grad=True)
-        self.proj = nn.Conv2d(dim, dim, 3, 1, 1, groups=dim)
+        att_dim = dim_out if dim_mul_in_att else dim
+        self.attn = MultiScaleAttention(
+            dim,
+            att_dim,
+            num_heads=num_heads,
+            input_size=input_size,
+            qkv_bias=qkv_bias,
+            kernel_q=kernel_q,
+            kernel_kv=kernel_kv,
+            stride_q=stride_q,
+            stride_kv=stride_kv,
+            norm_layer=norm_layer,
+            has_cls_embed=has_cls_embed,
+            mode=mode,
+            pool_first=pool_first,
+            rel_pos_spatial=rel_pos_spatial,
+            rel_pos_zero_init=rel_pos_zero_init,
+            residual_pooling=residual_pooling,
+        )
+        self.drop_path = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
+        self.norm2 = norm_layer(att_dim)
+        mlp_hidden_dim = int(att_dim * mlp_ratio)
+        self.has_cls_embed = has_cls_embed
+
+        mlp_dim_out = dim_out
+        self.mlp = IRB(
+            in_features=att_dim,
+            hidden_features=mlp_hidden_dim,
+            out_features=mlp_dim_out,
+        )
+        if dim != dim_out:
+            self.proj = nn.Linear(dim, dim_out)
+
+        if len(stride_q) > 0 and numpy.prod(stride_q) > 1:
+            kernel_skip = [s + 1 if s > 1 else s for s in stride_q]
+            stride_skip = stride_q
+            padding_skip = [int(skip // 2) for skip in kernel_skip]
+            self.pool_skip = nn.MaxPool2d(
+                kernel_skip, stride_skip, padding_skip, ceil_mode=False
+            )
+        else:
+            self.pool_skip = None
 
     def forward(self, x):
         B, N, C = x.shape
-        H = W = int(N**0.5)
-        res = x
-        x = x + self.drop_path(self.attn(self.norm1(x), H, W))
-        x = x + self.drop_path(self.mlp(self.norm2(x), H, W))
-        x = x + (res * self.gamma)
+        H = W = int(N ** 0.5)
+        hw_shape = [H, W]
+        x_norm = self.norm1(x)
+        x_block, hw_shape_new = self.attn(x_norm, hw_shape)
+
+        if self.dim_mul_in_att and self.dim != self.dim_out:
+            x = self.proj(x_norm)
+        x_res, _ = attention_pool(
+            x, self.pool_skip, hw_shape, has_cls_embed=self.has_cls_embed
+        )
+        x = x_res + self.drop_path(x_block)
+        x_norm = self.norm2(x)
+        x_mlp = self.mlp(x_norm, H, W)
+
+        if not self.dim_mul_in_att and self.dim != self.dim_out:
+            x = self.proj(x_norm)
+        x = x + self.drop_path(x_mlp)
         return x
 
 
 if __name__ == '__main__':
     x = torch.rand(2, 64, 64).cuda()
-    m = Block(64).cuda()
+    m = Block(64, 64).cuda()
     o = m(x)
     print(o.shape)
